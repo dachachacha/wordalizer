96b0cefccae286018fe30e8dbc4e4da042e4a413
dkenna <dingo.mckenna@gmail.com>
2018-12-04 13:12:12 +0200

    first!

---
+++ create_pages.py
@@ -0,0 +1,43 @@
+import sys
+from random import shuffle, SystemRandom
+
+words_per_line = 15
+char_per_line = 55
+lines_per_page = 35
+bag_of_words = open('words.txt',encoding='utf-8').read().split("\n")
+shuffle(bag_of_words)
+cnt = 0
+line = []
+lines = []
+char_cnt = 0
+line_cnt = 0
+for i in bag_of_words:
+    char_cnt += len(i)
+    cnt += 1
+    line.append(i)
+    #if cnt == words_per_line or char_cnt > char_per_line:
+    if char_cnt > char_per_line:
+        char_cnt = 0
+        cnt = 0
+        lines.append(" ".join(line))
+        line = []
+        line_cnt += 1
+    if line_cnt > lines_per_page:
+        lines.append("")
+        line_cnt = 0
+
+text = "\n".join(lines)
+title = []
+for i in range(3):
+    r = SystemRandom().randint(0,len(bag_of_words))
+    title.append(bag_of_words[r])
+import string
+title = " ".join(title)
+translator = title.maketrans('', '', string.punctuation)
+title = title.translate(translator)
+open('text.txt','w',encoding='utf-8').write(title + "\n\n" + text)
+    
+#quit()
+#for i in lines:
+#    print(i)
+#    #sys.stdout.buffer.write(i.encode())
---
+++ process_data.py
@@ -0,0 +1,59 @@
+import os
+import collections
+from random import shuffle
+
+
+def avoid(fname):
+    if fname in ['.DS_Store']: return True
+    else: return False
+
+files = {}
+
+counter_of_words = collections.Counter()
+bag_of_words = []
+
+for i in os.listdir('./diary/'):
+    if (avoid(i)) : continue
+    if i.endswith('.txt') or i == 'shrooms':
+        print(i)
+        #lines = open('diary/' + i,'rb').readlines()
+        sentences = [ x.strip() for x in open('diary/' + i,encoding='utf-8').readlines() ]
+        files[i] =  " ".join([x.strip() for x in [ " ".join(x.split()) for x in sentences ]])
+        elements = [ x.strip() for x in files[i].split(" ") ]
+        counter_of_words.update(elements)
+        bag_of_words.extend(elements)
+    if i == 'diary_end_7_2016':
+        files[i] = {}
+        files[i]['dir'] = i
+        files[i]['files'] = {}
+        for j in os.listdir('diary/' + i):
+            if (avoid(j)) : continue
+            print ("processing: " + j)
+            sentences = [ x.strip() for x in open('diary/' + i + '/' + j,encoding='utf-8').readlines() ]
+            files[i]['files'][j] = " ".join([ x.strip() for x in [ " ".join(x.split()) for x in sentences ]])
+            elements = [ x.strip() for x in files[i]['files'][j].split(" ") ]
+            bag_of_words.extend(elements)
+            counter_of_words.update(elements)
+
+for i in files.keys():
+    print(i)
+    if type(files[i]) == dict:
+        for j in files[i]['files'].keys():
+            print('\t' + j)
+
+shuffle(bag_of_words)
+#print(counter_of_words)
+
+keys = list(dict(counter_of_words).keys())
+counter_of_words2 = {}
+shuffle(keys)
+for i in keys:
+    counter_of_words2[i] = counter_of_words[i]
+
+import json
+with open('wordcount.json', 'w', encoding='utf-8') as fp:
+    json.dump(counter_of_words2, fp, indent=4, sort_keys=True, ensure_ascii=False)
+with open('words.txt', 'w', encoding='utf-8') as fp:
+    for i in bag_of_words:
+        fp.write("%s\n" % i)
+#print(counter_of_words)
